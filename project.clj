80eeb4d5597999dc8f867d9f66199efbcfa73164
Mark McGranaghan <mmcgrana@gmail.com>
2011-08-19 01:10:18 -0700

    latest compojure version

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :dependencies [
     [org.clojure/clojure "1.2.1"]
     [clj-json "0.3.1"]
-    [compojure "0.4.1"]
+    [compojure "0.6.5"]
     [ring-basic-auth "0.1.0"]
     [ring/ring-jetty-adapter "0.3.10"]])
