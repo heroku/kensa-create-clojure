e30d6e54db8f3dbc800757547ea06216588b632d
Lincoln Stoll <lstoll@lstoll.net>
2011-12-16 19:54:00 -0800

    Upgrading to clojure 1.3 + other deps.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject addon "0.0.1"
   :dependencies [
-    [org.clojure/clojure "1.2.1"]
-    [clj-json "0.3.1"]
-    [compojure "0.6.5"]
+    [org.clojure/clojure "1.3.0"]
+    [clj-json "0.4.3"]
+    [compojure "1.0.0-RC1"]
     [ring-basic-auth "0.1.0"]
-    [hiccup "0.3.6"]
-    [ring/ring-jetty-adapter "0.3.11"]])
+    [hiccup "0.3.7"]
+    [clj-http "0.2.6"]
+    [ring/ring-jetty-adapter "1.0.0"]])
